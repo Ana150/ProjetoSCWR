78bd37d7a3ebbdfbff9a24d5508e96f676d9a785
Ana150 <79976673+Ana150@users.noreply.github.com>
2023-03-14 11:43:39 -0300

    14/03

---
+++ aula1.R
@@ -0,0 +1,41 @@
+#MEU PRIMEIRO CÓDIGO EM R
+
+#Exemplo de soma
+3+2
+
+#Exemplo subtração
+5-7
+
+#Exemplo de multiplicação
+5*4
+
+#Divisão
+10/2
+
+#Potencia
+3^3
+
+3**3
+
+#Raiz quadrada
+sqrt(100)
+
+#Raiz enésima
+8^(1/3)
+
+#Módulo
+abs(-7)
+abs(7)
+
+#Resto de divisão
+9%%3
+
+#Quociente da divisão
+9%/%3
+
+#Fatorial
+factorial(300)
+
+#Logaritmo
+log(8, base=2)
+log(8,2)
